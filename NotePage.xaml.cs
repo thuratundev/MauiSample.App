cf19ac744a2a66a6f8cc8b928a59b7dc991677ad
Thura Tun <thura.kofat@gmail.com>
2022-08-24 10:03:55 +0630

    InitialState App

---
+++ NotePage.xaml.cs
@@ -0,0 +1,29 @@
+namespace HelloMaui;
+
+public partial class NotePage : ContentPage
+{
+    string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+    public NotePage()
+	{
+		InitializeComponent();
+
+        if (File.Exists(_fileName))
+            TextEditor.Text = File.ReadAllText(_fileName);
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Save the file.
+        File.WriteAllText(_fileName, TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        // Delete the file.
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        TextEditor.Text = string.Empty;
+    }
+}
